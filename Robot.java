2b7ad3706c89b8e8aa57b44bb1badde541e21c5f
sefikaozturk <64414733+sefikaozturk@users.noreply.github.com>
2021-01-07 18:02:32 +0300

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2020 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -14,13 +7,7 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.button.JoyStickButton;
 import edu.wpi.first.wpilibj.DigitalInput;
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends TimedRobot {
   
   public static final int kMotorPort = 0;
@@ -38,18 +25,11 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     m_motor = new PWMVictorSPX(kMotorPort);
     m_joystick = new JoyStick(kJoyStick);
-    m_button1 = new JoyStickButton(m_joystick, 1); //the button with no.1 in joystick
+    m_button1 = new JoyStickButton(m_joystick, 1); 
     m_button2 = new JoyStickButton(m_joystick, 2);
     input = new DigitalInput(kInputPort);
   }
 
- @Override
-  public void teleopInit() {
-  }
-
-  /**
-   * This function is called periodically during operator control.
-   */
   @Override
   public void teleopPeriodic() {
     while(input.get()){
@@ -68,5 +48,3 @@ public class Robot extends TimedRobot {
 
 }
 
-
-//ground = şase
